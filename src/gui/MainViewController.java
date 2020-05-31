23e34f261d8c3f16a2d07224201af71303a47a19
Herhos <herhos@outlook.com>
2020-05-31 14:53:59 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,14 +36,17 @@ public class MainViewController implements Initializable
 	@FXML
 	public void onActionMenuItemDepartment()
 	{
-		//loadView("/gui/DepartmentList.fxml");
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->
+			{
+				controller.setDepartmentService(new DepartmentService());
+				controller.updateTableView();
+			});
 	}
 	
 	@FXML
 	public void onActionMenuItemAbout()
 	{
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
@@ -52,7 +56,7 @@ public class MainViewController implements Initializable
 	}
 	
 	// Método para carregar a tela about dentro da tela principal.
-	private synchronized void loadView(String absoluteName)
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> intitializingAction)
 	{
 		try
 		{
@@ -68,40 +72,14 @@ public class MainViewController implements Initializable
 			mainVbox.getChildren().clear();
 			mainVbox.getChildren().add(mainMenu);
 			mainVbox.getChildren().addAll(newVbox.getChildren());
+			
+			T controller = loader.getController();
+			intitializingAction.accept(controller);
 		}
 		catch (IOException e)
 		{
 			Alerts.showAlert("IO Exception", "Erro ao carregar a tela!",
 				e.getMessage(), AlertType.ERROR);
 		}
-	}
-	
-	// Método para carregar a tela provisoriamente.
-		private synchronized void loadView2(String absoluteName)
-		{
-			try
-			{
-				FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-				VBox newVbox = loader.load();
-				
-				Scene mainScene = Main.getMainScene();
-				// getRoot pega o primeiro elemento da view
-				VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-				
-				// Código para incluir os filhos da tela about na tela principal
-				Node mainMenu = mainVbox.getChildren().get(0);
-				mainVbox.getChildren().clear();
-				mainVbox.getChildren().add(mainMenu);
-				mainVbox.getChildren().addAll(newVbox.getChildren());
-				
-				DepartmentListController controller = loader.getController();
-				controller.setDepartmentService(new DepartmentService());
-				controller.updateTableView();
-			}
-			catch (IOException e)
-			{
-				Alerts.showAlert("IO Exception", "Erro ao carregar a tela!",
-					e.getMessage(), AlertType.ERROR);
-			}
-		}
+	}	
 }
